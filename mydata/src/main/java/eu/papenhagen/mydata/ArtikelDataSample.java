beb1b1e51f6948db3de9a24a0dd37590141423a1
Jens Papenhagen <jenspapenhagen@users.noreply.github.com>
2017-10-25 14:53:51 +0200

    build nicer jxml

--- mydata/src/main/java/eu/papenhagen/mydata/ArtikelDataSample.java
+++ mydata/src/main/java/eu/papenhagen/mydata/ArtikelDataSample.java
@@ -19,12 +19,14 @@ public class ArtikelDataSample {
         Artikel a = new Artikel(11385, "Computer Maus", 100, 15.80);
         Artikel a1 = new Artikel(12588, "Computer Tastatur", 450, 75.99);
         Artikel a2 = new Artikel(22864, "Mauspad", 50, 2.50);
-        
-        
-        
+        Artikel a3 = new Artikel(22344, "ACER Laptop", 3850, 850.99);
+        Artikel a4 = new Artikel(12344, "Laptoptasche 15 Zoll", 50, 35.00);
+
         list.add(a);
         list.add(a1);
         list.add(a2);
+        list.add(a3);
+        list.add(a4);
         
         return list;
     }
