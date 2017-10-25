42a1b63286cc8b8d1b9deb7585c58c3e9081f030
Jens Papenhagen <jenspapenhagen@users.noreply.github.com>
2017-10-25 11:33:08 +0200

    cleanup the jasper report

--- myreport/src/main/java/eu/papenhagen/myreport/Reporter.java
+++ myreport/src/main/java/eu/papenhagen/myreport/Reporter.java
@@ -8,10 +8,13 @@ package eu.papenhagen.myreport;
 import eu.papenhagen.mydata.ArtikelDataSample;
 import eu.papenhagen.mydata.PersonDataSample;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -26,37 +29,54 @@ import net.sf.jasperreports.swing.JRViewer;
  * @author jens.papenhagen
  */
 public class Reporter {
-    
-    public static JasperPrint writeReport() throws JRException{
+
+    public static JasperPrint writeReport() throws JRException {
         //yes hardcoded path
         //TODO better path 
-        String location = "Blank_A4.jrxml";
+        String location = "Rechnung.jrxml";
         Objects.requireNonNull(location);
-        
+
         JasperReport compileReport = JasperCompileManager.compileReport(location);
         Map<String, Object> data = new HashMap<>();
+        
+        //person
         data.put("anrede", "Herr");
-        data.put("vorname", PersonDataSample.get().get(0).getFirstName() );
-        data.put("nachname", PersonDataSample.get().get(0).getLastName() );
+        data.put("vorname", PersonDataSample.get().get(0).getFirstName());
+        data.put("nachname", PersonDataSample.get().get(0).getLastName());
+        data.put("strasse", PersonDataSample.get().get(0).getStreet());
+        data.put("hausnummer", PersonDataSample.get().get(0).getHousenumber());
+        data.put("plz", PersonDataSample.get().get(0).getZip());
+        data.put("stadt", PersonDataSample.get().get(0).getCity());
+        
+        //rechnungs "api"
+        data.put("rechnungsnr", 155);
+        data.put("kundennr", 16351315);
         
         
         
-          
+
         JRDataSource datasource = new JRBeanCollectionDataSource(ArtikelDataSample.get());
         return JasperFillManager.fillReport(compileReport, data, datasource);
-        
-        
+
     }
-    
+
     public static void main(String[] args) throws JRException {
-        
-        JFrame f = new JFrame("Viewer");
-        f.getContentPane().setLayout(new BorderLayout());
-        f.getContentPane().add(new JRViewer( writeReport() ));
-        f.pack();
-        f.setVisible(true);
-        
-        
+        SwingUtilities.invokeLater(() -> {
+            JFrame f = new JFrame("Viewer");
+            f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            f.setPreferredSize(new Dimension(600, 800));
+            f.getContentPane().setLayout(new BorderLayout());
+            try {
+                f.getContentPane().add(new JRViewer(writeReport()));
+            } catch (JRException ex) {
+                
+            }
+            f.pack();
+            f.setVisible(true);
+
+        }
+        );
+
     }
-    
+
 }
